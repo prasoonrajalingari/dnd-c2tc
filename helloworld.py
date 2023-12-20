1740fa3d15454771bbe16b1aa008797aef6779ed
Sravani Venkatreddy gari <rajalingari.prasoon@gmail.com>
2023-12-20 18:19:38 -0500

    first commiting

---
+++ helloworld.py
@@ -0,0 +1,26 @@
+import pyflowchart
+
+
+def choosefruit(fruitname):
+    if fruitname == 'apple':
+        a= 5
+        b= 5
+        c = a+b
+        print (f'{c}')
+        print(f'you have choosen Apple')
+    else:
+        print(f'you have choosen {fruitname}')
+
+
+def foo(a, b):
+    if a:
+        print("a")
+        a = 'apple'
+    else:
+        for i in range(3):
+            print("b")
+            b= 'banana'
+    choosefruit(b)
+    return a 
+
+foo(True,False)
